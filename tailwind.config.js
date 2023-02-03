f57a958f340da3cb687bcfab23d33621e757b523
hiboedi <hi.boedi8@gmail.com>
2023-02-03 10:09:21 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+        goyang: "goyang 1s ease-in-out infinite",
+      },
+      keyframes: {
+        goyang: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
